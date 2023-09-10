937baee2b427210f3b904c9605520ff555de27ea
ltphuc093 <lethanhphuc093@gmail.com>
2023-09-10 08:47:57 +0700

    Chinh lai icon

--- AppMobile/baseapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ AppMobile/baseapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import platform_device_id
 import platform_device_id_macos
 import shared_preferences_foundation
-import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
